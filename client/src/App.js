e149430994ee938eba2057f16e16c5c5931efc0c
Danny Choi <choi.danny20@gmail.com>
2020-09-23 11:57:07 -0500

    added routers to switch to different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
-    <div>
-      <h1>Project 3</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
   );
 }
