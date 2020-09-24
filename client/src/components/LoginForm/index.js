da3a2c45f8f214016bbfd22cdccd1c9d15da0a53
Evan <evanmohammedsaleh@gmail.com>
2020-09-24 12:03:36 -0500

    added codes to login

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,27 +1,37 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./style.css";
 
 export default function LoginForm() {
-  return (
-    <div className="container">
+  const passwordInput = useRef();
+  const emailInput = useRef();
+  function login() {
+    emailInput.current.value;
+    passwordInput.current.value;
+    console.log(emailInput)
+    console.log(passwordInput)
+  }
+
+    return (
+      <div className="container">
       <form>
         <div className="row">
           <div className="input-field col s12">
-            <input id="email" type="email" className="validate" />
+            <input id="email" type="email" className="validate" ref={emailInput}/>
             <label for="email">Email</label>
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-            <input id="password" type="password" className="validate" />
+            <input id="password" type="password" className="validate" ref={passwordInput} />
             <label for="password">Password</label>
           </div>
         </div>
         <button
+          onClick={(event) => {login(event)}}
           class="btn waves-effect waves-light"
           type="submit"
           name="action"
-        >
+          >
           Sign Up
         </button>
       </form>
