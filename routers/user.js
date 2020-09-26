b8a5d6970b9d07057db71f85f436de472b38e798
Evan <evanmohammedsaleh@gmail.com>
2020-09-26 08:50:45 -0500

    fixed PackageJson

--- routers/user.js
+++ routers/user.js
@@ -16,51 +16,51 @@ router.post("/users/signup", async (req, res) => {
 });
 
 router.post("/users/login", async (req, res) => {
-    //Login a registered user
-    try {
-      const { email, password } = req.body;
-      const user = await User.findByCredentials(email, password);
-      if (!user) {
-        return res
-          .status(401)
-          .send({ error: "Login failed! Check authentication credentials" });
-      }
-      const token = await user.generateAuthToken();
-      res.send({ user, token });
-    } catch (error) {
-      res.status(400).send(error);
+  //Login a registered user
+  try {
+    const { email, password } = req.body;
+    const user = await User.findByCredentials(email, password);
+    if (!user) {
+      return res
+        .status(401)
+        .send({ error: "Login failed! Check authentication credentials" });
     }
-  });
-router.get('/users/me', auth, async(req, res) => {
-    // View logged in users
-    res.send(req.user)
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.get('/users/me', auth, async (req, res) => {
+  // View logged in users
+  res.send(req.user)
 })
-router.get('/users', auth, async(req, res) => {
-    // View logged in users
-    res.send(req.user)
+router.get('/users', auth, async (req, res) => {
+  // View logged in users
+  res.send(req.user)
 })
 router.post('/users/me/logout', auth, async (req, res) => {
-    // Log user out of the application
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token != req.token
-        })
-        await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send(error)
-    }
+  // Log user out of the application
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token != req.token
+    })
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
-router.post('/users/me/logoutall', auth, async(req, res) => {
-    // Log user out of all devices
-    try {
-        req.user.tokens.splice(0, req.user.tokens.length)
-        await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send(error)
-    }
+router.post('/users/me/logoutall', auth, async (req, res) => {
+  // Log user out of all devices
+  try {
+    req.user.tokens.splice(0, req.user.tokens.length)
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
 
