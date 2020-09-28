94e44756295222203272f1f24679151b1652c170
Evan <evanmohammedsaleh@gmail.com>
2020-09-28 12:30:24 -0500

    Added Logout Button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import axios from "axios";
 
 export default function Navbar() {
+  function logOut(event) {
+    event.preventDefault();
+    localStorage.removeItem('token')
+    window.location.replace(`/login`)
+  }
   return (
     <ul>
       <li>
@@ -10,6 +16,18 @@ export default function Navbar() {
       </li>
       <li>
         <Link to="/chatlogin">Chatlogin</Link>
+      </li>
+      <li>
+      <button
+          onClick={(event) => {
+            logOut(event);
+          }}
+          class="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+        >
+          Log out
+        </button>
       </li>
     </ul>
   );
