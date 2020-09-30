69dd3f72208d79085d843ff28f5c9a07cbe028b0
Danny Choi <choi.danny20@gmail.com>
2020-09-30 17:35:17 -0500

    able to target the src attribute of each image liked

--- client/src/components/ImageSearch/index.js
+++ client/src/components/ImageSearch/index.js
@@ -6,8 +6,9 @@ import "./style.css";
 export default function ImageSearch() {
   const [searchImagesState, setSearchImagesState] = useState({
     searchImages: [],
-    likeImage: false,
   });
+
+  const [like, setLike] = useState(false);
 
   function onSubmit(e) {
     e.preventDefault();
@@ -21,13 +22,19 @@ export default function ImageSearch() {
         searchImages: res.data.results,
       });
     });
-    console.log(...searchImagesState.searchImages);
   }
 
   useEffect(() => {
     let elems = document.querySelectorAll(".materialboxed");
     M.Materialbox.init(elems);
   });
+
+  function likeImage(e) {
+    console.log(
+      e.target.parentNode.parentNode.firstChild.firstChild.getAttribute("src")
+    );
+    setLike(!like);
+  }
 
   return (
     <div>
@@ -52,16 +59,11 @@ export default function ImageSearch() {
 
                 <a
                   className={
-                    searchImagesState.likeImage
+                    like
                       ? "btn-floating halfway-fab waves-effect waves-light red"
                       : "btn-floating halfway-fab waves-effect waves-light black"
                   }
-                  onClick={() =>
-                    setSearchImagesState({
-                      ...searchImagesState,
-                      likeImage: true,
-                    })
-                  }
+                  onClick={likeImage}
                 >
                   <i className="material-icons">favorite</i>
                 </a>
