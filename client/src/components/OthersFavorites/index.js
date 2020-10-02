07727a5482dbe6ba7b5ee4314bfaab6f8107295d
Danny Choi <choi.danny20@gmail.com>
2020-10-02 10:34:48 -0500

    mapped user specific images

--- client/src/components/OthersFavorites/index.js
+++ client/src/components/OthersFavorites/index.js
@@ -1,38 +1,52 @@
 import React, { useEffect, useState } from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import axios from "axios";
+import SideNav from "../SideNav";
 
-import FavoritesRender from "../FavoriteRender";
+// import FavoritesRender from "../FavoriteRender";
 // import Favorite from "../../../../models/Favorite";
 
 export default function OtherFavorites() {
-  const [ otherFav , setOtherFav] = useState ({
-    collection : [],
-  })
+  const [otherFav, setOtherFav] = useState({
+    collection: [],
+  });
   const config = {
     headers: {
       Authorization: localStorage.token,
     },
   };
 
+  const { id } = useParams();
 
-  const {id} = useParams()
+  useEffect(() => {
+    console.log(id);
+    axios
+      .get("/others/" + id)
 
-      useEffect (() => {
-        console.log(id)
-        axios.get("/others/" + id)
-        
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => console.log(err));
-      }, [id])
-        
+      .then((res) => {
+        console.log(res.data);
+        setOtherFav({
+          ...otherFav,
+          collection: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
   return (
     <div>
-      <h1></h1>
-      <FavoritesRender userFavorite={otherFav}/>
-     
+      <SideNav />
+
+      {otherFav.collection.map((image) => {
+        return (
+          <img
+            src={image.favImageURL}
+            alt={image.favImageURL}
+            key={image._id}
+          />
+        );
+      })}
+      {/* <FavoritesRender userFavorite={otherFav}/> */}
     </div>
   );
 }
