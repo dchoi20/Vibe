7fedc5ed0f08fc597999e3f40ba8e71251d3dcb5
Danny Choi <choi.danny20@gmail.com>
2020-10-02 12:33:02 -0500

    minor change to the catch all in server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/searchimages/:query", (req, res) => {
   });
 });
 
-app.get((req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
